287f2d003040168a40dbd0f890f93e9b461e8e78
mbentley <mbentley@mbentley.net>
2014-02-01 15:39:28 -1000

    added tomcat7

---
+++ ubuntu/tomcat7/readme.md
@@ -0,0 +1,10 @@
+# Apache Tomcat7
+
+Apache Tomcat 7.0.47
+
+* `docker build -t tomcat7 .`
+* `docker run tomcat7`
+
+Ports
+
+* 8080
---
+++ ubuntu/tomcat7/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ADMIN_USER=${ADMIN_USER:-admin}
+ADMIN_PASS=${ADMIN_PASS:-tomcat}
+MAX_UPLOAD_SIZE=${MAX_UPLOAD_SIZE:-52428800}
+
+cat << EOF > /opt/tomcat/conf/tomcat-users.xml
+<?xml version='1.0' encoding='utf-8'?>
+<tomcat-users>
+<user username="${ADMIN_USER}" password="${ADMIN_PASS}" roles="admin-gui,manager-gui"/>
+</tomcat-users>
+EOF
+
+#sed -i "s#.*max-file-size.*#\t<max-file-size>${MAX_UPLOAD_SIZE}</max-file-size>#g" /opt/tomcat/webapps/manager/WEB-INF/web.xml
+#sed -i "s#.*max-request-size.*#\t<max-request-size>${MAX_UPLOAD_SIZE}</max-request-size>#g" /opt/tomcat/webapps/manager/WEB-INF/web.xml
+
+/bin/sh -e /opt/tomcat/bin/catalina.sh run

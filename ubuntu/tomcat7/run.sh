99219b8a7ea32f7c8428dba2bc704c69a52fc898
mbentley <mbentley@mbentley.net>
2014-02-02 02:44:58 -1000

    vanilla tomcat7 (v7.0.50)

--- ubuntu/tomcat7/run.sh
+++ ubuntu/tomcat7/run.sh
@@ -10,7 +10,7 @@ cat << EOF > /opt/tomcat/conf/tomcat-users.xml
 </tomcat-users>
 EOF
 
-#sed -i "s#.*max-file-size.*#\t<max-file-size>${MAX_UPLOAD_SIZE}</max-file-size>#g" /opt/tomcat/webapps/manager/WEB-INF/web.xml
-#sed -i "s#.*max-request-size.*#\t<max-request-size>${MAX_UPLOAD_SIZE}</max-request-size>#g" /opt/tomcat/webapps/manager/WEB-INF/web.xml
+sed -i "s#.*max-file-size.*#\t<max-file-size>${MAX_UPLOAD_SIZE}</max-file-size>#g" /opt/tomcat/webapps/manager/WEB-INF/web.xml
+sed -i "s#.*max-request-size.*#\t<max-request-size>${MAX_UPLOAD_SIZE}</max-request-size>#g" /opt/tomcat/webapps/manager/WEB-INF/web.xml
 
 /bin/sh -e /opt/tomcat/bin/catalina.sh run

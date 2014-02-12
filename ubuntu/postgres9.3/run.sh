e343dd5f26d1cf3f5ed9f856e999cdf65a5b1cbf
mbentley <mbentley@mbentley.net>
2014-02-12 16:49:14 -0500

    updated postgres configuration from the ground up

--- ubuntu/postgres9.3/run.sh
+++ ubuntu/postgres9.3/run.sh
@@ -6,43 +6,19 @@ POSTGRESQL_PASS=${POSTGRESQL_PASS:-"docker"}
 POSTGRESQL_DB=${POSTGRESQL_DB:-"docker"}
 
 POSTGRESQL_BIN_PATH=/usr/lib/postgresql/9.3/bin
+POSTGRESQL_INITDB=/usr/lib/postgresql/9.3/bin/initdb
 POSTGRESQL_CONFIG_FILE=/etc/postgresql/9.3/main/postgresql.conf
+POSTGRESQL_DATA=/var/lib/postgresql/9.3/main
 
-POSTGRESQL_DATA=/data
-POSTGRESQL_CFGDIR=/etc/postgresql/9.3/main
-POSTGRESQL_DBDIR=/var/lib/postgresql/9.3/main
-
-chmod 755 ${POSTGRESQL_DATA}
-
-if [ ! -d "${POSTGRESQL_DATA}/db" ]
-then
-	echo "no data; moving..."
-	mkdir -p ${POSTGRESQL_DATA}/db
-	mv ${POSTGRESQL_DBDIR} ${POSTGRESQL_DATA}/db
-	ln -s ${POSTGRESQL_DATA}/db/main ${POSTGRESQL_DBDIR}
-	chown -R postgres:postgres ${POSTGRESQL_DATA}/db
-else
-	echo "using existing data..."
-	rm -rf ${POSTGRESQL_DBDIR}
-	ln -s ${POSTGRESQL_DATA}/db/main ${POSTGRESQL_DBDIR}
+# initialize db if needed
+if [ ! "`ls -A $POSTGRESQL_DATA`" ] ; then
+	chown -R postgres $POSTGRESQL_DATA
+	su postgres sh -c "$POSTGRESQL_INITDB $POSTGRESQL_DATA"
 fi
 
-if [ ! -d "${POSTGRESQL_DATA}/cfg" ]
-then
-	echo "no config; moving..."
-	mkdir -p ${POSTGRESQL_DATA}/cfg
-	mv ${POSTGRESQL_CFGDIR} ${POSTGRESQL_DATA}/cfg
-	ln -s ${POSTGRESQL_DATA}/cfg/main ${POSTGRESQL_CFGDIR}
-	chown -R postgres:postgres ${POSTGRESQL_DATA}/cfg
+su postgres /bin/bash -c "$POSTGRESQL_BIN_PATH/postgres --single -c config_file=$POSTGRESQL_CONFIG_FILE" <<< "CREATE USER $POSTGRESQL_USER WITH SUPERUSER PASSWORD '$POSTGRESQL_PASS';"
+su postgres /bin/bash -c "$POSTGRESQL_BIN_PATH/postgres --single -c config_file=$POSTGRESQL_CONFIG_FILE" <<< "CREATE DATABASE $POSTGRESQL_DB WITH OWNER $POSTGRESQL_USER ENCODING 'utf8' TEMPLATE template0;"
 
-	/usr/bin/sudo -u postgres $POSTGRESQL_BIN_PATH/postgres --single --config-file=$POSTGRESQL_CONFIG_FILE <<< "CREATE USER $POSTGRESQL_USER WITH SUPERUSER;"
-	/usr/bin/sudo -u postgres $POSTGRESQL_BIN_PATH/postgres --single --config-file=$POSTGRESQL_CONFIG_FILE <<< "ALTER USER $POSTGRESQL_USER WITH PASSWORD '$POSTGRESQL_PASS';"
-	/usr/bin/sudo -u postgres $POSTGRESQL_BIN_PATH/postgres --single --config-file=$POSTGRESQL_CONFIG_FILE <<< "CREATE DATABASE $POSTGRESQL_DB OWNER $POSTGRESQL_USER ENCODING 'utf8' TEMPLATE template0;"
-	echo 'host all all 0.0.0.0/0 md5' >> /etc/postgresql/9.3/main/pg_hba.conf
-else
-	echo "using existing config..."
-	rm -rf ${POSTGRESQL_CFGDIR}
-	ln -s ${POSTGRESQL_DATA}/cfg/main ${POSTGRESQL_CFGDIR}
-fi
-
+echo 'host all all 0.0.0.0/0 md5' >> /etc/postgresql/9.3/main/pg_hba.conf
+echo "listen_addresses='*'" >> /etc/postgresql/9.3/main/postgresql.conf
 /usr/bin/supervisord

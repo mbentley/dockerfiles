34a837f1f7f74112d9794f4430e088030501dc98
Matt Bentley <mbentley@mbentley.net>
2014-03-20 04:28:17 -0900

    updated PtokaX

--- ubuntu/ptokax/run.sh
+++ ubuntu/ptokax/run.sh
@@ -5,12 +5,16 @@ PASSWORD=${PASSWORD:-password}
 
 if [ ! -d /opt/PtokaX/cfg ]
 then
-mv /opt/PtokaX/cfg.example /opt/PtokaX/cfg
+cp -r /opt/PtokaX/cfg.example /opt/PtokaX/cfg
 
 cat << EOF > /opt/PtokaX/cfg/Settings.xml
 <?xml version="1.0" encoding="windows-1252" standalone="yes" ?>
 <PtokaX Version="0.5.0.1">
-    <Booleans />
+    <Booleans>
+        <Bool Name="AutoRegister">0</Bool>
+        <Bool Name="AdvancedPassProtection">0</Bool>
+        <Bool Name="HashPasswords">1</Bool>
+    </Booleans>
     <Integers />
     <Strings>
         <String Name="HubName">$HUB</String>
@@ -29,6 +33,10 @@ cat << EOF > /opt/PtokaX/cfg/RegisteredUsers.xml
         <Profile>0</Profile><!-- 0 for master, 1 for operator, 2 for vip, 3 for reg -->
     </RegisteredUser>
 </RegisteredUsers>
+EOF
+
+cat << EOF > /opt/PtokaX/cfg/Motd.txt
+Welcome to PtokaX ($HUB)
 EOF
 fi
 

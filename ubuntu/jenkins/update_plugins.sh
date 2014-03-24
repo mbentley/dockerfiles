b2cd6d8ec6c90952062f09907b57d6c82d23c150
Matt Bentley <mbentley@mbentley.net>
2014-03-24 09:00:54 -0400

    added 'ssh' plugin; updated plugins

--- ubuntu/jenkins/update_plugins.sh
+++ ubuntu/jenkins/update_plugins.sh
@@ -4,7 +4,7 @@ SCRIPT_HOME="$( cd "$( dirname "$0" )" && pwd )"
 
 cd $SCRIPT_HOME/plugins
 
-plugins="cobertura greenballs instant-messaging ircbot postbuild-task copy-to-slave credentials ssh-credentials ssh-agent git-client git github-api github scm-api mercurial grails"
+plugins="cobertura greenballs instant-messaging ircbot postbuild-task copy-to-slave credentials ssh-credentials ssh ssh-agent git-client git github-api github scm-api mercurial grails"
 
 for i in ${plugins}
 do

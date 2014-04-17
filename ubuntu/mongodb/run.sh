2c69a3baf3d3baa2f1ce6d1a6934f8510a44346f
Matt Bentley <mbentley@mbentley.net>
2014-04-16 20:25:34 -0400

    updated run file

--- ubuntu/mongodb/run.sh
+++ ubuntu/mongodb/run.sh
@@ -3,13 +3,13 @@
 MONGODB_USER=${MONGODB_USER:-"admin"}
 MONGODB_PASS=${MONGODB_PASS:-"mongo"}
 
-if [ ! -f /data/db/.mongo_root_init ]
+if [ ! -f /data/db/mongodb_root_init ]
 then
 	echo "Configuring ${MONGODB_USER} as 'root' role..."
 	/opt/mongodb/bin/mongod --fork --logpath /var/log/mongodb.log
 	mongo admin --eval 'db.createUser({user: "'"${MONGODB_USER}"'", pwd: "'"${MONGODB_PASS}"'", roles: [ "root" ]});'
 	mongo admin --eval 'db.shutdownServer();'
-	touch /data/db/.mongo_root_init
+	touch /data/db/mongodb_root_init
 fi
 
 /usr/bin/supervisord

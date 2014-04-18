1334dda592729475b828e3d6a44bb61b14031389
Matt Bentley <mbentley@mbentley.net>
2014-04-18 18:19:47 -0400

    updates to rabbitmq

--- ubuntu/rabbitmq/run.sh
+++ ubuntu/rabbitmq/run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-RMQ_USER=${RMQ_USER:-guest}
-RMQ_PASS=${RMQ_PASS:-guest}
+#RMQ_USER=${RMQ_USER:-guest}
+#RMQ_PASS=${RMQ_PASS:-guest}
+
+chown rabbitmq:rabbitmq /var/lib/rabbitmq
+
+if [ ! -f /etc/rabbitmq/rabbitmq.config ]
+then
+	echo '[{rabbit, [{loopback_users, []}]}].' > /etc/rabbitmq/rabbitmq.config
+fi
 
 /usr/bin/supervisord
